c3571fbb2463e625813a6d6278390a53e5bef0d2
joao-lucas-dias <joao.lucasdias@hotmail.com>
2023-05-02 15:52:45 +0100

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			Features Pricing Resources Login Sign Up More than just shorter links Build your
+			brand’s recognition and get detailed insights on how your links are performing. Get
+			Started Shorten a link here... Shorten It! Advanced Statistics Track how your links
+			are performing across the web with our advanced statistics dashboard. Brand
+			Recognition Boost your brand recognition with each click. Generic links don’t mean a
+			thing. Branded links help instil confidence in your content. Detailed Records Gain
+			insights into who is clicking your links. Knowing when and where people engage with
+			your content helps inform better decisions. Fully Customizable Improve brand
+			awareness and content discoverability through customizable links, supercharging
+			audience engagement. Boost your links today Get Started Features Link Shortening
+			Branded Links Analytics Resources Blog Developers Support Company About Our Team
+			Careers Contact
+		</>
+	);
 }
 
-export default App
+export default App;
