e261ced97d4873bf5398271c5552fdd140f262c6
joao-lucas-dias <joao.lucasdias@hotmail.com>
2023-05-03 16:35:55 +0100

    Add secondary cta

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import Hero from "./components/Hero/Hero";
 import Main from "./components/Main/Main";
+import Cta from "./components/Cta/CTA";
 import Footer from "./components/Footer/Footer";
 
 const App = () => {
 	return (
 		<>
 			<Hero />
-			Features Pricing Resources Login Sign Up Shorten a link here... Shorten It! 
+			Features Pricing Resources Login Sign Up Shorten a link here... Shorten It!
 			<Main />
-			Boost your links today Get Started
+			<Cta />
 			<Footer />
 		</>
 	);
---
+++ src/components/Cta/Cta.tsx
@@ -0,0 +1,12 @@
+import classes from "./CTA.module.css";
+
+const Cta = () => {
+	return (
+		<section className={classes.secondary_cta}>
+			<h2>Boost your links today</h2>
+			<button>Get Started</button>
+		</section>
+	);
+};
+
+export default Cta;
