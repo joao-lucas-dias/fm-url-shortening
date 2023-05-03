0d2e28af526bf6cdf73622f1588df7c88f4f925c
joao-lucas-dias <joao.lucasdias@hotmail.com>
2023-05-04 00:12:33 +0100

    Add form and results list

---
+++ models/types.ts
@@ -0,0 +1,4 @@
+export default interface Result {
+  originalUrl: string;
+  shortUrl: string;
+}
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,25 +1,52 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 import classes from "./Form.module.css";
+import ResultItem from "./ResultItem/ResultItem";
+import Result from "../../../models/types";
 
 const Form = () => {
-  const [enteredUrl, setEnteredUrl] = useState("");
+	const [enteredUrl, setEnteredUrl] = useState("");
+	const [resultsList, setResultsList] = useState<Result[]>([]);
 
-  const formSubmitHandler = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  };
+	const formSubmitHandler = async (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
 
-  const inputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setEnteredUrl(event.target.value);
-  };
+		const response = await fetch(`https://api.shrtco.de/v2/shorten?url=${enteredUrl}`);
+
+		const resultUrl: string = await response
+			.json()
+			.then((value) => value.result.full_short_link);
+
+		setResultsList((oldList) => [
+			...oldList,
+			{ originalUrl: enteredUrl, shortUrl: resultUrl }
+		]);
+
+		setEnteredUrl("");
+	};
+
+	const inputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+		setEnteredUrl(event.target.value);
+	};
 
 	return (
-		<>
+		<div className={classes.wrapper}>
 			<form onSubmit={formSubmitHandler} className={classes.form}>
-				<input type="text" onChange={inputChangeHandler} placeholder="Shorten a link here..." />
+				<input
+					type="text"
+					value={enteredUrl}
+					onChange={inputChangeHandler}
+					placeholder="Shorten a link here..."
+				/>
 				<button type="submit">Shorten It!</button>
 			</form>
-			{false && <ul className={classes.results_list}></ul>}
-		</>
+			{resultsList.length > 0 && (
+				<ul className={classes.results_list}>
+					{resultsList.map((result) => (
+						<ResultItem key={result.originalUrl} data={result} />
+					))}
+				</ul>
+			)}
+		</div>
 	);
 };
 
