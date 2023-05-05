d3ca412da7441a797be92fdc2d8765d56648ce77
joao-lucas-dias <joao.lucasdias@hotmail.com>
2023-05-05 19:18:41 +0100

    Fix nav toggle behaviour

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,37 +1,51 @@
-import { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 
 import classes from "./Header.module.css";
 
 const Header = () => {
-  const [showNav, setShowNav] = useState(false);
+	const navigation = document.querySelector(`.${classes.nav_menu}`);
 
-  const navClickHandler = () => {
-    setShowNav((oldValue) => !oldValue);
-  };
+	const closeNavigationOnResize = () => {
+		const screenWidth = window.innerWidth;
+
+		if (screenWidth > 767 && navigation?.classList.contains(classes.show)) {
+			navigation.classList.remove(classes.show);
+		}
+	};
+
+	window.addEventListener("resize", closeNavigationOnResize);
+
+	const toggleHandler = () => {
+		const classList = navigation?.classList;
+		console.log(classList?.value);
+
+		if (classList?.contains(classes.show)) {
+			classList.remove(classes.show);
+		} else {
+			classList?.add(classes.show);
+		}
+	};
 
 	return (
 		<header className={classes.header}>
-      <img src="/images/logo.svg" alt="Logo." />
-			<button onClick={navClickHandler} className={classes.hamburger}>
+			<img src="/images/logo.svg" alt="Logo." />
+			<button onClick={toggleHandler} className={classes.hamburger}>
 				<MenuIcon fontSize="large" />
 			</button>
-			{showNav && (
-				<div className={classes.nav_menu}>
-					<ul>
-						<li>Features</li>
-						<li>Pricing</li>
-						<li>Resources</li>
-					</ul>
-					<span></span>
-					<ul>
-						<li>Login</li>
-						<li>
-							<button className={classes.button}>Sign Up</button>
-						</li>
-					</ul>
-				</div>
-			)}
+			<div className={classes.nav_menu}>
+				<ul>
+					<li>Features</li>
+					<li>Pricing</li>
+					<li>Resources</li>
+				</ul>
+				<span></span>
+				<ul>
+					<li>Login</li>
+					<li>
+						<button className={classes.button}>Sign Up</button>
+					</li>
+				</ul>
+			</div>
 		</header>
 	);
 };
